13e652f203ba78bc481584903d4b603c895b834e
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2026-04-09 18:57:20 +0200

    fix: removed unused imports

--- src/main/java/org/lessons/spring_la_mia_pizzeria_relazioni/controllers/DiscountController.java
+++ src/main/java/org/lessons/spring_la_mia_pizzeria_relazioni/controllers/DiscountController.java
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping("/discounts")
